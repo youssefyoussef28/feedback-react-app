90e896fe5b8ba13d3bc97cd1c37a3511c7748d8b
Youssef Youssef <youssefyoussef28@gmail.com>
2022-10-25 22:50:30 +0300

    Props

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <Header text="Hello" />
+      <Header />
       <div className="container">
         <h1>My App</h1>
       </div>

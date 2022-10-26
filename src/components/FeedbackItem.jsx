49b048f24ce09f7c3bfd96c18fab5dfee7add033
Youssef Youssef <youssefyoussef28@gmail.com>
2022-10-26 20:03:18 +0300

    Prop drilling and Applied the Filter Array Method

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import Card from "./shared/Card";
 import PropTypes from "prop-types";
+import { FaTimes } from "react-icons/fa";
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
   return (
-    <Card reverse={true}>
+    <Card reverse={false}>
       <div className="num-display">{item.rating}</div>
+      <button
+        onClick={() => {
+          handleDelete(item.id);
+        }}
+        className="close"
+      >
+        <FaTimes color="purple" />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );

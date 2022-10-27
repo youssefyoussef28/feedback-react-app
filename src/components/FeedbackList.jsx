7af3ec9c8537adb56131a439409287591a5e0ca0
Youssef Youssef <youssefyoussef28@gmail.com>
2022-10-27 13:55:43 +0300

    Framer motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
 import PropTypes from "prop-types";
@@ -9,12 +10,34 @@ function FeedbackList({ feedback, handleDelete }) {
 
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 }
+
+// Return List with no animation Below.
+// return (
+//   <div className="feedback-list">
+//     {feedback.map((item) => (
+//       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+//     ))}
+//   </div>
+// );
 
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
